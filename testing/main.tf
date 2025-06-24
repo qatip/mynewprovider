5ad2e2f928bea5618060e44f4d9477a7334771ce
qatip <michael2697@hotmail.com>
2025-06-24 20:44:48 +0100

    Update

--- testing/main.tf
+++ testing/main.tf
@@ -1,16 +1,1 @@
-terraform {
-  required_providers {
-    mynewprovider = {
-      source  = "custom/mynewprovider"
-      version = "0.1.0"
-    }
-  }
-}
-
-provider "mynewprovider" {}
-
-resource "mynewprovider_task" "example" {
-  title       = "From custom provider"
-  description = "Created using mynewprovider"
-  completed   = false
-}
+#
